60f7488e48aedfdd1f745b8dab5caf3da8d72b04
Alex Gonzalez <71677050+Adgonzalez2018@users.noreply.github.com>
2020-10-27 13:02:49 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,28 +6,31 @@ import pandas as pd
 # opening and reading the data and putting it in an array
 data = pd.read_csv("volts-vs-time.csv", sep=",")
 data = data[["v", "t", "tdiff"]]
+
+# x = time, y = volts, z = difference between each time lapped,
 y, x, z = data["t"], data["v"], data["tdiff"]
+# standard deviation of time
 std_x = x.std()
+# standard deviation of voltage
 std_y = y.std()
-max_x = np.max(x)
-# standard deviation of difference in time
+# standard deviation of difference in time (which is used in the graph)
 std_z = z.std()
 
-# average x and y, standard deviation for time and volts
+# prints average x and y, standard deviation for time and volts and time difference
 print("Average Voltage: ", x.mean())
 print("Average Time: ", y.mean())
 print("Standard Deviation for Voltage: ", x.std())
 print("Standard Deviation for Time: ", y.std())
 print("Standard Deviation for the difference in Time collected: ", z.std())
 
-# putting it into the matplotlib
+# putting time and voltage into graph
 style.use("grayscale")
 pyplot.suptitle('Time vs Voltage', fontsize= 20)
 pyplot.scatter(x, y, color="navy")
 
-# error bar graphs the uncertainty of each point in accordance to time
+# error bar graphs the uncertainty of the difference in time
 pyplot.errorbar(x, y, yerr=std_z, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
-
+# labels
 pyplot.xlabel('Voltage (v)')
 pyplot.ylabel('Time (s)')
 pyplot.show()
--- main2.py
+++ main2.py
@@ -6,30 +6,38 @@ import pandas as pd
 # opening and reading the data and putting it in an array
 data = pd.read_csv("volts-vs-time.csv", sep=",")
 data = data[["v", "t", "tdiff", "log"]]
+# x = time, y = volts (unused in this file), z = difference between each time lapped, w = ln(v)
 x, y, z, w = data["t"], data["v"], data["tdiff"], data["log"]
+
 std_x = x.std()
 std_y = y.std()
+# standard deviation of ln(v)
 std_w = w.std()
-
 # standard deviation of difference in time
 std_z = z.std()
-# average x and y, standard deviation for time and volts
+# average x and y, standard deviation for time and ln(v)
 m,b = np.polyfit(x, w, 1)
 print("Average Time: ", x.mean())
 print("Average ln(v): ", w.mean())
+
+# printing standard deviation
 print("Standard Deviation for Time: ", std_x)
 print("Standard Deviation for Voltage: ", std_y)
 print("Standard Deviation for the difference in Time collected: ", std_z)
 print("Standard Deviation for ln(V): ", std_w)
 print("Slope:", m)
-# putting it into the matplotlib
+
+# putting time and ln(v) into graph
 style.use("grayscale")
 pyplot.suptitle('ln(V) vs. Time', fontsize= 20)
 pyplot.scatter(x, w, color="navy")
-# error bar graphs the uncertainty of each point in accordance to time
+
+# error bar graphs the uncertainty of each point in the y-axis for ln(v)
 pyplot.errorbar(x, w, yerr=std_w, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
 
+# plots the slope
 pyplot.plot(x, m*x+b, color="green")
+# labels
 pyplot.xlabel('Time (s)')
 pyplot.ylabel('ln(V) (v)')
 pyplot.show()
