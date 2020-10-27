1b3603728c60cfe4b748926cd53bb93da3f20150
Alex Gonzalez <71677050+Adgonzalez2018@users.noreply.github.com>
2020-10-27 11:27:31 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -9,23 +9,25 @@ data = data[["v", "t", "tdiff"]]
 y, x, z = data["t"], data["v"], data["tdiff"]
 std_x = x.std()
 std_y = y.std()
-
+max_x = np.max(x)
 # standard deviation of difference in time
 std_z = z.std()
+
 # average x and y, standard deviation for time and volts
 print("Average Voltage: ", x.mean())
 print("Average Time: ", y.mean())
 print("Standard Deviation for Voltage: ", x.std())
 print("Standard Deviation for Time: ", y.std())
 print("Standard Deviation for the difference in Time collected: ", z.std())
+
 # putting it into the matplotlib
-style.use("ggplot")
+style.use("grayscale")
 pyplot.suptitle('Time vs Voltage', fontsize= 20)
 pyplot.scatter(x, y, color="navy")
+
 # error bar graphs the uncertainty of each point in accordance to time
 pyplot.errorbar(x, y, yerr=std_z, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
-m,b = np.polyfit(x, y, 1)
-pyplot.plot(x, m*x+b, color="black")
-pyplot.xlabel('Time (s)')
-pyplot.ylabel('Voltage (v)')
+
+pyplot.xlabel('Voltage (v)')
+pyplot.ylabel('Time (s)')
 pyplot.show()
--- main2.py
+++ main2.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as pyplot
 from matplotlib import style
 import numpy as np
 import pandas as pd
-import math
 
 # opening and reading the data and putting it in an array
 data = pd.read_csv("volts-vs-time.csv", sep=",")
@@ -15,20 +14,22 @@ std_w = w.std()
 # standard deviation of difference in time
 std_z = z.std()
 # average x and y, standard deviation for time and volts
+m,b = np.polyfit(x, w, 1)
 print("Average Time: ", x.mean())
-print("Average Voltage: ", y.mean())
+print("Average ln(v): ", w.mean())
 print("Standard Deviation for Time: ", std_x)
 print("Standard Deviation for Voltage: ", std_y)
 print("Standard Deviation for the difference in Time collected: ", std_z)
 print("Standard Deviation for ln(V): ", std_w)
+print("Slope:", m)
 # putting it into the matplotlib
-style.use("ggplot")
-pyplot.suptitle('ln(v) vs. Time', fontsize= 20)
+style.use("grayscale")
+pyplot.suptitle('ln(V) vs. Time', fontsize= 20)
 pyplot.scatter(x, w, color="navy")
 # error bar graphs the uncertainty of each point in accordance to time
-pyplot.errorbar(x, w, xerr=std_z, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
-m,b = np.polyfit(x, w, 1)
-pyplot.plot(x, m*x+b, color="black")
+pyplot.errorbar(x, w, yerr=std_w, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
+
+pyplot.plot(x, m*x+b, color="green")
 pyplot.xlabel('Time (s)')
-pyplot.ylabel('ln(v) (v)')
+pyplot.ylabel('ln(V) (v)')
 pyplot.show()
