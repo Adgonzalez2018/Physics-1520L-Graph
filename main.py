183269cbfff798c5866d8f095dd63f1253ad1d9b
Alex Gonzalez <71677050+Adgonzalez2018@users.noreply.github.com>
2020-10-27 10:33:14 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import pandas as pd
 # opening and reading the data and putting it in an array
 data = pd.read_csv("volts-vs-time.csv", sep=",")
 data = data[["v", "t", "tdiff"]]
-x, y, z = data["t"], data["v"], data["tdiff"]
+y, x, z = data["t"], data["v"], data["tdiff"]
 std_x = x.std()
 std_y = y.std()
 
 # standard deviation of difference in time
 std_z = z.std()
 # average x and y, standard deviation for time and volts
-print("Average Time: ", x.mean())
-print("Average Voltage: ", y.mean())
-print("Standard Deviation for Time: ", x.std())
-print("Standard Deviation for Voltage: ", y.std())
+print("Average Voltage: ", x.mean())
+print("Average Time: ", y.mean())
+print("Standard Deviation for Voltage: ", x.std())
+print("Standard Deviation for Time: ", y.std())
 print("Standard Deviation for the difference in Time collected: ", z.std())
 # putting it into the matplotlib
 style.use("ggplot")
-pyplot.suptitle('Voltage vs. Time', fontsize= 20)
+pyplot.suptitle('Time vs Voltage', fontsize= 20)
 pyplot.scatter(x, y, color="navy")
 # error bar graphs the uncertainty of each point in accordance to time
-pyplot.errorbar(x, y, xerr=std_z, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
+pyplot.errorbar(x, y, yerr=std_z, fmt='o', ecolor='navy', elinewidth=1, capsize=2)
 m,b = np.polyfit(x, y, 1)
 pyplot.plot(x, m*x+b, color="black")
 pyplot.xlabel('Time (s)')
